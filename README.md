b433a1df114cfa3df562356bd6d9b46550025bc3
Anurag Kumar <anuragkumarrr@gmail.com>
2022-12-29 12:18:28 -0600

    Established mongoDB connection with help of Mongoose & express to create endpoints

---
+++ README.md
@@ -0,0 +1,7 @@
+# Getting Started with INoteBook
+
+- This project is used to keep track of all the notes on clould.
+- Technologies used : MongoDB,Express.js,React.js,Node.js
+
+# Notes from the project
+- 
---
+++ backendapi/db.js
@@ -0,0 +1,18 @@
+// getting-started.js
+const mongoose = require('mongoose');
+// const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI = "mongodb://localhost:27017/"
+
+
+
+async function connectToMongo(){
+   await mongoose.connect(mongoURI,(err)=>{
+    if(!err){
+        console.log('Connect to mongoDB Successfully')
+    }else{
+        console.log('Connect to mongoDB Unsuccessfully')
+     }
+   })
+}
+
+module.exports = connectToMongo;
