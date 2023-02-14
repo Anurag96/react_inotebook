655c5f9416d1f7c62902cf48bd6204910dcb2eaf
Anurag Kumar <c-anurag.kumar@charter.com>
2023-02-14 14:19:29 -0600

    Using useContext data transfer to any component

--- inotebook/src/context/notes/NoteContextProvider.js
+++ inotebook/src/context/notes/NoteContextProvider.js
@@ -1,10 +1,33 @@
-import React, { useState,createContext } from "react";
+import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteContextProvider = (props) => {
 
+    const notesInitial = [
+        {
+          "_id": "63ebe5bbbab515d41ef6b8bf",
+          "user": "63ebe420bab515d41ef6b8bd",
+          "title": "02/14-Todays Tasks",
+          "description": "Go to the gym after the office",
+          "tag": "general",
+          "date": "2023-02-14T19:49:15.366Z",
+          "__v": 0
+        },
+        {
+          "_id": "63ebe677def5d5531829c323",
+          "user": "63ebe420bab515d41ef6b8bd",
+          "title": "02/14-Todays Tasks",
+          "description": "Cook Dinner after gym",
+          "tag": "general",
+          "date": "2023-02-14T19:52:23.924Z",
+          "__v": 0
+        }
+      ]
+
+      const [notes, setnotes] = useState(notesInitial)
+
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setnotes}}>
             {props.children}
         </NoteContext.Provider>
     )
